2d7fd17c72bfca0cc35561ef43d88c5fea2fac59
Cristian Disabato <crisdisabato1@gmail.com>
2022-05-29 18:21:54 -0300

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <h1>Disabato Ecommerce</h1>
-      <h2>Proximamente</h2>
+      <NavBar/>
+      <h2>
+        PROXIMAMENTE
+      </h2>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import './NavBar.css';
+function NavBar() {
+    return <header> 
+        <nav>
+            <ul className='navbar'>
+                <li><a className='fontnav' href="">STORE</a></li>
+                <li><a className='fontnav' href="">INICIO</a></li>
+                <li><a className='fontnav' href="">ABOUT US</a></li>
+                
+                
+            </ul>
+        </nav>
+    </header>;
+  }
+  
+  export default NavBar;
